8526b8e1e6ae5eedb87f467f49779a0207db1433
mschruf <michael.schruf@gmail.com>
2014-08-08 13:53:51 -0700

    Initial commit

---
+++ lib/n-choose-k.h
@@ -0,0 +1,9 @@
+#ifndef __N_CHOOSE_K_H
+#define __N_CHOOSE_K_H 1
+
+#include <stdint.h>  /* int64_t */
+
+/* calculate nCk, where n, k are integers 0 <= k <= n */
+int64_t n_choose_k(int n, int k);
+
+#endif /* !__N_CHOOSE_K_H */
